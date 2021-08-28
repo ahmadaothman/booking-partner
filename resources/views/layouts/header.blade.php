faaa293ea40577895e8013624d926b127a0edc56
ahmadaothman <ahmadaothman96@gmail.com>
2021-08-28 13:47:14 +0300

    .

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -20,9 +20,7 @@
 						<span class="user-name">Balance:  {{ Auth::user()->balance }}$</span>
 					</a>
 					<div class="dropdown-menu dropdown-menu-right">
-						<a class="dropdown-item" href="profile.php"><i class="fa fa-user-md" aria-hidden="true"></i> Profile</a>
 						<a class="dropdown-item" href="profile.php"><i class="fa fa-cog" aria-hidden="true"></i> Setting</a>
-						<a class="dropdown-item" href="faq.php"><i class="fa fa-question" aria-hidden="true"></i> Help</a>
 						<a class="dropdown-item" href="{{ route('logout') }}"  onclick="event.preventDefault();
 						document.getElementById('logout-form').submit();">
 							<i class="fa fa-sign-out" aria-hidden="true"></i> Log Out
