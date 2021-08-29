5555804657fcc0948f60ef934c005127f2b5f9ff
ahmadaothman <ahmadaothman96@gmail.com>
2021-08-29 19:10:37 +0300

    .

--- resources/views/transportation/vehicle_success.blade.php
+++ resources/views/transportation/vehicle_success.blade.php
@@ -11,152 +11,153 @@
                 </tr>
             </tbody>
         </table>
-
-        <table class="table table-sm mb-4 mt-2 table-bordered borderless">
-            <tbody>
-                <tr>
-                    <td class="p-3"><img style="max-height:70px" src="{{ asset('/images/logo.png') }}" alt=""></td>
-                    <td></td>
-                    <td></td>
-                    <td class="text-right align-middle p-3">Transportation Booking</td>
-                </tr>
-                <tr>
-                    <td class="text-center align-middle" colspan="4"><strong>
-                        
-                        @if ($booking_data['trip_type'] == 'one_way')
-                        One Way Trip
-                        @else
-                        Round Trip
-                        @endif
-                        </strong>
-                        <br>
-                        <strong># {{ $booking_number }}</strong>
-                    </td>
-                        
-                </tr>
-            </tbody>
-        </table>
-
-        <table class="table table-sm  table-bordered borderless">
-            <tbody>
-                <tr>
-                    <td colspan="4"><strong>Contact:Info</strong></td>
-                </tr>
-                <tr>
-                    <td>{{ $booking_data['firstname'] }} {{ $booking_data['middlename'] }} {{ $booking_data['lastname'] }}</td>
-                     <td><span>Address: </span> {{ $booking_data['address'] }} </td>
-                    <td><span>D.O.B: </span> {{ $booking_data['date_of_birthday'] }} </td>
-                    <td><span>Sex: </span> {{ $booking_data['sex'] }} </td>
-                </tr>
-                <tr>
-                    <td>{{ $booking_data['nationality'] }} -  {{ $booking_data['telephone'] }} </td>
-                    <td>{{ $booking_data['email'] }} </td>
-                    <td><span>Passport No: </span>{{ $booking_data['passport_number'] }} </td>
-                    <td></td>
-                </tr>
-            </tbody>
-        </table>
-
-        <table class="table table-sm  table-bordered ">
-            <tbody>
-                <tr>
-                    <td class="text-center align-middle" colspan="4"><strong>Trip Location</strong></td>
-                </tr>
-                <tr>
-                    <td>From:</td>
-                    <td>{{ $from }}</td>
-                    <td>To:</td>
-                    <td>{{ $to }}</td>
-                </tr>
-                <tr>
-                    <td>Date:</td>
-                    <td>{{ $pickup_date }}</td>
-                    <td>Time:</td>
-                    <td>{{ $pickup_time }}</td>
-                </tr>
-            </tbody>
-        </table>
-        @if($booking_data['trip_type'] != 'one_way')
-            <table class="table table-sm  table-bordered ">
+        <div id="toprint" >
+            <table class="table table-sm mb-4 mt-2 table-bordered borderless">
                 <tbody>
                     <tr>
-                        <td class="text-center align-middle" colspan="4"><strong>Return Trip Location</strong></td>
+                        <td class="p-3"><img style="max-height:70px" src="{{ asset('/images/logo.png') }}" alt=""></td>
+                        <td></td>
+                        <td></td>
+                        <td class="text-right align-middle p-3">Transportation Booking</td>
                     </tr>
                     <tr>
-                        <td>From:</td>
-                        <td>{{ $return_from }}</td>
-                        <td>To:</td>
-                        <td>{{ $return_to }}</td>
-                    </tr>
-                    <tr>
-                        <td>Date:</td>
-                        <td>{{ $return_date }}</td>
-                        <td>Time:</td>
-                        <td>{{ $return_time }}</td>
+                        <td class="text-center align-middle" colspan="4"><strong>
+                            
+                            @if ($booking_data['trip_type'] == 'one_way')
+                            One Way Trip
+                            @else
+                            Round Trip
+                            @endif
+                            </strong>
+                            <br>
+                            <strong># {{ $booking_number }}</strong>
+                        </td>
+                            
                     </tr>
                 </tbody>
             </table>
-        @endif
 
-        <table class="table table-sm  table-bordered ">
-            <tbody>
-                <tr>
-                    <td class="text-center align-middle" colspan="4"><strong>Trip Vehicle</strong></td>
-                </tr>
-                <tr>
-                    <td>Vehicle:</td>
-                    <td>{{ $one_way_vehicle }}</td>
-                    <td>Price:</td>
-                    <td>{{ $one_way_vehicle_price }}$</td>
-                </tr>
-             
-            </tbody>
-        </table>
+            <table class="table table-sm  table-bordered borderless">
+                <tbody>
+                    <tr>
+                        <td colspan="4"><strong>Contact:Info</strong></td>
+                    </tr>
+                    <tr>
+                        <td>{{ $booking_data['firstname'] }} {{ $booking_data['middlename'] }} {{ $booking_data['lastname'] }}</td>
+                        <td><span>Address: </span> {{ $booking_data['address'] }} </td>
+                        <td><span>D.O.B: </span> {{ $booking_data['date_of_birthday'] }} </td>
+                        <td><span>Sex: </span> {{ $booking_data['sex'] }} </td>
+                    </tr>
+                    <tr>
+                        <td>{{ $booking_data['nationality'] }} -  {{ $booking_data['telephone'] }} </td>
+                        <td>{{ $booking_data['email'] }} </td>
+                        <td><span>Passport No: </span>{{ $booking_data['passport_number'] }} </td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
 
-        @if($booking_data['trip_type'] != 'one_way')
-        <table class="table table-sm  table-bordered ">
-            <tbody>
-                <tr>
-                    <td class="text-center align-middle" colspan="4"><strong>Return Trip Vehicle</strong></td>
-                </tr>
-                <tr>
-                    <td>Vehicle:</td>
-                    <td>{{ $round_vehicle }}</td>
-                    <td>Price:</td>
-                    <td>{{ $round_vehicle_price }}$</td>
-                </tr>
-             
-            </tbody>
-        </table>
-    @endif
-    <table class="table table-sm  table-bordered ">
-        <tbody>
-            <tr>
-                <td class="text-center align-middle" colspan="10"><strong>Pessengers</strong></td>
-            </tr>
-            @foreach ($pessengers as $pessenger)
-                <tr>
-                    <td>First Name:</td>
-                    <td>{{ $pessenger['firstname'] }}</td>
-                    <td>Middle Name:</td>
-                    <td>{{ $pessenger['middlename'] }}</td>
-                    <td>Last Name:</td>
-                    <td>{{ $pessenger['lastname'] }}</td>
-                    <td>Sex:</td>
-                    <td>{{ $pessenger['sex'] }}</td>
-                    <td>Nationality:</td>
-                    <td>{{ $pessenger['nationality'] }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-    <table class="table table-bordered">
-        <tbody>
-            <tr>
-                <td class="text-center align-middle"><h2>Amount To Paid: {{ $total }}$</h2></td>
-            </tr>
-        </tbody>
-    </table>
+            <table class="table table-sm  table-bordered ">
+                <tbody>
+                    <tr>
+                        <td class="text-center align-middle" colspan="4"><strong>Trip Location</strong></td>
+                    </tr>
+                    <tr>
+                        <td>From:</td>
+                        <td>{{ $from }}</td>
+                        <td>To:</td>
+                        <td>{{ $to }}</td>
+                    </tr>
+                    <tr>
+                        <td>Date:</td>
+                        <td>{{ $pickup_date }}</td>
+                        <td>Time:</td>
+                        <td>{{ $pickup_time }}</td>
+                    </tr>
+                </tbody>
+            </table>
+            @if($booking_data['trip_type'] != 'one_way')
+                <table class="table table-sm  table-bordered ">
+                    <tbody>
+                        <tr>
+                            <td class="text-center align-middle" colspan="4"><strong>Return Trip Location</strong></td>
+                        </tr>
+                        <tr>
+                            <td>From:</td>
+                            <td>{{ $return_from }}</td>
+                            <td>To:</td>
+                            <td>{{ $return_to }}</td>
+                        </tr>
+                        <tr>
+                            <td>Date:</td>
+                            <td>{{ $return_date }}</td>
+                            <td>Time:</td>
+                            <td>{{ $return_time }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            @endif
+
+            <table class="table table-sm  table-bordered ">
+                <tbody>
+                    <tr>
+                        <td class="text-center align-middle" colspan="4"><strong>Trip Vehicle</strong></td>
+                    </tr>
+                    <tr>
+                        <td>Vehicle:</td>
+                        <td>{{ $one_way_vehicle }}</td>
+                        <td>Price:</td>
+                        <td>{{ $one_way_vehicle_price }}$</td>
+                    </tr>
+                
+                </tbody>
+            </table>
+
+            @if($booking_data['trip_type'] != 'one_way')
+                <table class="table table-sm  table-bordered ">
+                    <tbody>
+                        <tr>
+                            <td class="text-center align-middle" colspan="4"><strong>Return Trip Vehicle</strong></td>
+                        </tr>
+                        <tr>
+                            <td>Vehicle:</td>
+                            <td>{{ $round_vehicle }}</td>
+                            <td>Price:</td>
+                            <td>{{ $round_vehicle_price }}$</td>
+                        </tr>
+                    
+                    </tbody>
+                </table>
+            @endif
+            <table class="table table-sm  table-bordered ">
+                <tbody>
+                    <tr>
+                        <td class="text-center align-middle" colspan="10"><strong>Pessengers</strong></td>
+                    </tr>
+                    @foreach ($pessengers as $pessenger)
+                        <tr>
+                            <td>First Name:</td>
+                            <td>{{ $pessenger['firstname'] }}</td>
+                            <td>Middle Name:</td>
+                            <td>{{ $pessenger['middlename'] }}</td>
+                            <td>Last Name:</td>
+                            <td>{{ $pessenger['lastname'] }}</td>
+                            <td>Sex:</td>
+                            <td>{{ $pessenger['sex'] }}</td>
+                            <td>Nationality:</td>
+                            <td>{{ $pessenger['nationality'] }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            <table class="table table-bordered">
+                <tbody>
+                    <tr>
+                        <td class="text-center align-middle"><h2>Amount To Paid: {{ $total }}$</h2></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
 <style>
